817bba48b10c237b6048c248b16e27bb14a22b2d
Naveen001-max <ns4483382@gmail.com>
2026-09-20 20:28:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ClientPulse from '../clientpulse_glass.jsx'
+import ClientPulse from '../zenvoy.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
